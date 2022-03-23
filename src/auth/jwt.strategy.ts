85c1c8c4586348fbbca2079959207711ae4664ae
Irfan Rafif <mirfanrafif17@gmail.com>
2022-03-23 22:17:21 +0700

    console log user

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -16,6 +16,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
   async validate(payload: UserJwtPayload) {
     const user = await this.authService.findUser(payload.id);
+    console.log(user);
     if (user !== undefined) {
       return user;
     }
