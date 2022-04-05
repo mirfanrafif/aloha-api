14f18c108f33b080229e188a4653c08b8334fd91
Irfan Rafif <mirfanrafif17@gmail.com>
2022-04-05 16:28:45 +0700

    env secret

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -10,7 +10,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: true,
-      secretOrKey: 'sebuah_secret',
+      secretOrKey: process.env.JWT_SECRET,
     });
   }
 
