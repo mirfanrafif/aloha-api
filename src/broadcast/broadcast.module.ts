5f1c554c026198ee2844e4cd725e6e47a6c6d7a6
rafif <mirfanrafif17@gmail.com>
2023-03-04 13:49:43 +0700

    fix broadcast

--- src/broadcast/broadcast.module.ts
+++ src/broadcast/broadcast.module.ts
@@ -3,10 +3,21 @@ import { WablasModule } from 'src/core/wablas/wablas.module';
 import { CustomerCrmModule } from 'src/core/pukapuka/customer-crm.module';
 import { MessageBroadcastController } from './broadcast-message.controller';
 import { BroadcastMessageService } from './broadcast-message.service';
+import { CustomerModule } from 'src/customer/customer.module';
+import { MessageGateway } from 'src/messages/gateways/message.gateway';
+import { MessageRepositoryModule } from 'src/core/repository/message/message.module';
+import { MessageHelper } from 'src/messages/helper/message.helper';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [WablasModule, CustomerCrmModule],
+  imports: [
+    WablasModule,
+    CustomerCrmModule,
+    CustomerModule,
+    MessageRepositoryModule,
+    UserModule,
+  ],
   controllers: [MessageBroadcastController],
-  providers: [BroadcastMessageService],
+  providers: [BroadcastMessageService, MessageGateway, MessageHelper],
 })
 export class BroadcastMessageModule {}
